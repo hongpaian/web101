caf7ce95918cf3d47524dbe9217e313c13c2045d
hongpaian <huudanh2366@gmail.com>
2022-12-03 20:10:01 +0700

    raw code

---
+++ app.js
@@ -0,0 +1,33 @@
+function change1(){
+    let a = document.getElementById("imgitems1")
+    a.src =  "https://nld.mediacdn.vn/2019/4/22/photo-1-1555933420437981632888.jpg" 
+}
+function change2(){
+    let a = document.getElementById("imgitems1")
+    a.src =  "https://img.meta.com.vn/Data/image/2021/08/06/hoang-hon-2.jpg" 
+}
+function change3(){
+    let a = document.getElementById("imgitems2")
+    a.src =  "https://matkinhtamduc.com/wp-content/uploads/2021/04/can-ap-dung-nhung-cach-bao-ve-mat-de-luon-co-mot-doi-mat-khoe-537x400.jpg" 
+}
+function change4(){
+    let a = document.getElementById("imgitems2")
+    a.src =  "https://kfoods.com/daily-tips/icons/033_03365.jpg" 
+}
+function change5(){
+    let a = document.getElementById("imgitems3")
+    a.src =  "https://image.shutterstock.com/image-vector/feel-better-hand-writing-script-260nw-1676422108.jpg" 
+}
+function change6(){
+    let a = document.getElementById("imgitems3")
+    a.src =  "https://media.istockphoto.com/vectors/progress-bar-with-inscription-better-days-loading-and-sun-with-cloud-vector-id1072634124?k=20&m=1072634124&s=612x612&w=0&h=HpqkG03Oaa3vxmOgN3aHB0oSAli3mkh5O2EJsDuig1k=" 
+}
+function change7(){
+    let a = document.getElementById("txt").value
+    if (a == "" ){
+        alert("Bạn hãy chia sẻ điều gì đó")
+    }
+    else{
+        alert("Cảm ơn bạn đã chia sẻ")
+    }
+}
